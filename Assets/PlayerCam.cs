f9cbf96c8000b7fd77a3169f44bb1022778308c3
JackMilewski <Jackmilewski49@gmail.com>
2024-12-09 10:52:03 -0500

    Initial commit

---
+++ Assets/PlayerCam.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRoatation;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        //get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRoatation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRoatation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRoatation, 0);
+    }
+}
---
+++ Assets/UI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] Text location;
+
+    void Start()
+    {
+        location.text = "jhkbgfj0";
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
